573a0ccf38984f585f15c6ee0daf87b51dab1d15
San Diego Mesa College <sdmesasoftware@sdccd.edu>
2025-09-08 19:50:55 -0700

    Update to Script DamageOnKey

--- Module_01/Assets/_Scripts/Gameplay/DamageOnKey.cs
+++ Module_01/Assets/_Scripts/Gameplay/DamageOnKey.cs
@@ -1,18 +1,14 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DamageOnKey : MonoBehaviour
 {
     [SerializeField] private HealthComponent target;
     [SerializeField] private int damageAmount = 10;
-    //[SerializeField] private KeyCode key = KeyCode.K;
     [SerializeField] private Key key = Key.K;
-}
 
     void Update()
     {
-
-        //if (Input.GetKeyDown(key)) target?.Damage(damageAmount);
-
         if (Keyboard.current[key].wasPressedThisFrame)
 
         {
